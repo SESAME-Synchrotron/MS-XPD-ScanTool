58f46e0dc2fce43f62f1d3c9c03dbc72ada9ff04
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2021-11-21 13:33:51 +0200

    update

--- testSlitsOperations.py
+++ testSlitsOperations.py
@@ -4,5 +4,5 @@ import log
 
 log.setup_custom_logger("./SED_MS_Scantool.log")
 log.info("Start scanning tool")
-x= slitsOperations("/Users/mustafaalzubi/Desktop/DCA/_Dev/2thetaCalculator/testDataset/CAF32_18k_10s_3_20.0000.tiff", 3, "/Users/mustafaalzubi/Desktop/DCA/Beamlines/MS/DAQ/XRD-Scan/configrations/2theta-Slits-Step.json")
+x= slitsOperations(imgPath="/Users/mustafaalzubi/Desktop/DCA/_Dev/2thetaCalculator/testDataset/CAF32_18k_10s_3_20.0000.tiff", tTheta=3, configFile="/Users/mustafaalzubi/Desktop/DCA/Beamlines/MS/DAQ/XRD-Scan/configrations/2theta-Slits-Step.json")
 print (*x.slitsPixelIntinisty)
