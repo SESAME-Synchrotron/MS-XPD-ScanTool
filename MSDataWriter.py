f1871d100e3bed6774efcee1e3e98f6fbded5062
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2021-12-13 00:38:59 +0200

    Update MSDataWriter.py

--- MSDataWriter.py
+++ MSDataWriter.py
@@ -114,6 +114,7 @@ class MSDataWriter:
 		intensityPlottingDataFile = open("intensity.txt","r")
 		twoTheta = numpy.array (twoThetaPlottingDataFile.readlines())
 		intensity = intensityPlottingDataFile.readlines()
+		plt.cla()
 		plt.plot(twoTheta, intensity)
 		plt.draw()
 		plt.pause(0.001)
