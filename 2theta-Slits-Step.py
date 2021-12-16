0b0f87ed0e42bd084eb987c034abccca83bb11a7
Mustafa-zubi <mostafa.zoubi@sesame.org.jo>
2021-12-16 19:58:45 +0200

    fixing minor issue

--- 2theta-Slits-Step.py
+++ 2theta-Slits-Step.py
@@ -60,7 +60,7 @@ class XRD:
 		#self.parser.add_argument('-name',  		type=str,  	help='Experiment  name')
 		self.parser.add_argument('-exptype',  	type=str,   default="local",		help='Experiment  type (local,users)')
 		self.parser.add_argument('-proposal',	type=int,	default=99999999,		help='Experiment  proposal number')      
-		self.parser.add_argument('-devMode',	type=int,	default="No",			help='development mode, yes means you can run with no Beam')      
+		self.parser.add_argument('-devMode',	type=str,	default="No",			help='development mode, yes means you can run with no Beam')      
 
 		self.args = self.parser.parse_args()
 
