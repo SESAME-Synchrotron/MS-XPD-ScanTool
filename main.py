23ce7f5afdda66d293de5d51526cb3f3c0de72e1
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-06-05 15:16:47 +0300

    update & enhancements

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3.9
 
 import epics
-import subprocess
+import os
+import sys
 from twoThetaStep import twoThetaStep
 from time import sleep
+from SEDSS.CLIMessage import CLIMessage
 
 if __name__ == "__main__":
 
-    command = 'tmux new -d -s MS_TwoThetaStep && tmux send-keys -t MS_TwoThetaStep "cd IOCs/MS_DAQ; ./bin/linux-x86_64/MS iocBoot/iocMS/st.cmd" ENTER'
+    # command = 'tmux new -d -s MS_TwoThetaStep && tmux send-keys -t MS_TwoThetaStep "cd IOCs/MS_DAQ; ./bin/linux-x86_64/MS iocBoot/iocMS/st.cmd" ENTER'
 
-    subprocess.run(['sh', '-c', command])
-    sleep(0.5)
-    twoThetaStep(["/home/dcasu/XRD-Scan/IOCs/MS_DAQ/db/MS_UI_settings.req", "/home/dcasu/XRD-Scan/IOCs/MS_DAQ/db/MS_settings.req", "/home/dcasu/XRD-Scan/IOCs/MS_DAQ/db/MS_TwoThetaStep_settings.req"], {"$(P)":"MS:", "$(N)":list(range(1, 41))})
+    # os.system(command)
+    # sleep(0.5)
+    
+    pvlist = ["/home/dcasu/XRD-Scan/IOCs/MS_DAQ/db/MS_UI_settings.req", "/home/dcasu/XRD-Scan/IOCs/MS_DAQ/db/MS_settings.req"]
+    macros = {"$(P)":"MS:", "$(N)":list(range(1, 41))}
+
+    os.system('cd & /home/dcasu/MStest -qt')
+
+    if epics.PV("MS:ScanningType").get() == 1:
+
+        command = 'tmux new -d -s MS_TwoThetaStep && tmux send-keys -t MS_TwoThetaStep "cd IOCs/MS_DAQ; ./bin/linux-x86_64/MS iocBoot/iocMS_TwoThetaStep/st.cmd" ENTER'
+        os.system(command)
+        sleep(0.5)
+        pvlist.append("/home/dcasu/XRD-Scan/IOCs/MS_DAQ/db/MS_TwoThetaStep_settings.req")
+        twoThetaStep(pvlist, macros)
+
+    else:
+        CLIMessage("Scanning type not provided!!", "E")
+        sys.exit()
