03b632b12dd094ce7df167260d6ba7b197508bb9
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-06-06 12:04:57 +0300

    integrate twoThetaScan with Slits in UI

--- UI/DAQ_Tool/MS/wizard.cpp
+++ UI/DAQ_Tool/MS/wizard.cpp
@@ -71,9 +71,20 @@ void Wizard::initializing()
     startLoading = No;
 
 //    if(configFile_ == 1)
-        ui->samplesButton->setEnabled(false);
-    ui->validIntervals->setHidden(true);
-    ui->validSamples->setHidden(true);
+    switch (scanningType->get().toInt()) {
+
+    case 1:
+            ui->samplesButton->setEnabled(false);
+        ui->validIntervals->setHidden(true);
+        ui->validSamples->setHidden(true);
+        break;
+
+    case 3:
+            ui->samplesButton3->setEnabled(false);
+        ui->validIntervals3->setHidden(true);
+        ui->validSamples3->setHidden(true);
+        break;
+    }
 }
 
 void Wizard::intervalsButtonClicked()
@@ -97,6 +108,7 @@ void Wizard::on_intervalsButton2_clicked()
 void Wizard::on_intervalsButton3_clicked()
 {
     // this button for twoThetaSlits Scan GUI
+    intervalsButtonClicked();
 }
 
 void Wizard::on_intervalsButton4_clicked()
@@ -136,22 +148,33 @@ int Wizard::nextId() const
             clearFields();
             return 6;
         }
-        else if(configFile_ == 2 and scanningType_ == 1)
+        else if(configFile_ == 2 and (scanningType_ == 1 or scanningType_ == 3))
             return 5;
         else if(configFile_ == 1 and scanningType_ == 3)
+        {
+            clearFields();
             return 8;
-        else if(configFile_ == 2 and scanningType_ == 3)
-            return 5;
+        }
         break;
 
     case 5:
         if(configFile_ == 2 and startLoading == 1)
         {
-            return 6;
+            if(scanningType_ == 1)
+                return 6;
+            else if(scanningType_ == 3)
+                return 8;
         }
         break;
 
     case 6:
+        if (intervals_ and samples_ and scans_ and expFileName_ and settlingTime_ and checkTable_ and checkSample_ and checkNSamples_)
+        {
+        return 10;
+        }
+        break;
+
+    case 8:
         if (intervals_ and samples_ and scans_ and expFileName_ and settlingTime_ and checkTable_ and checkSample_ and checkNSamples_)
         {
         return 10;
@@ -163,7 +186,7 @@ int Wizard::nextId() const
         return -1;
 
     default:
-        ;
+        cout<<1<<endl;
     }
 }
 
@@ -179,13 +202,28 @@ void Wizard::clearFields() const
 
     usleep(100000);
 
-    ui->intervals->clear();
-    ui->samples->clear();
-    ui->scans->clear();
-    ui->expFileName->clear();
-    ui->settlingTime->clear();
-    ui->userComments->clear();
-    ui->expComments->clear();
+    switch (scanningType_)
+    {
+    case 1:
+        ui->intervals->clear();
+        ui->samples->clear();
+        ui->scans->clear();
+        ui->expFileName->clear();
+        ui->settlingTime->clear();
+        ui->userComments->clear();
+        ui->expComments->clear();
+        break;
+
+    case 3:
+        ui->intervals3->clear();
+        ui->samples3->clear();
+        ui->scans3->clear();
+        ui->expFileName3->clear();
+        ui->settlingTime3->clear();
+        ui->userComments3->clear();
+        ui->expComments3->clear();
+        break;
+    }
 }
 
 void Wizard::checkStatus()
@@ -199,27 +237,58 @@ void Wizard::checkStatus()
    checkTable_     = checkTable->get().toBool();
    checkSample_    = checkSample->get().toBool();
 
-   if(checkTable_ == 1)
-       ui->validIntervals->setHidden(true);
-   else
-       ui->validIntervals->setHidden(false);
+   switch (scanningType_) {
+   case 1:
+       if(checkTable_ == 1)
+           ui->validIntervals->setHidden(true);
+       else
+           ui->validIntervals->setHidden(false);
+       break;
+
+   case 3:
+       if(checkTable_ == 1)
+           ui->validIntervals3->setHidden(true);
+       else
+           ui->validIntervals3->setHidden(false);
+       break;
+   }
+
 
 //   if(checkSample_ == 1)
 //       ui->validSamples->setHidden(true);
 //   else
 //       ui->validSamples->setHidden(false);
 
-   if(ui->samples->text().toInt() == samplesGUI->getCheckCount() and checkSample_ == 1)
-   {
-//       Client::writePV(MS_CheckSamples, Yes);
-       ui->validSamples->setHidden(true);
-       checkNSamples_ = Yes;
-   }
-   else
-   {
-//       Client::writePV(MS_CheckSamples, MS_CheckSamples_val);
-       ui->validSamples->setHidden(false);
-       checkNSamples_ = No;
+   switch (scanningType_) {
+   case 1:
+       if(ui->samples->text().toInt() == samplesGUI->getCheckCount() and checkSample_ == 1)
+       {
+    //       Client::writePV(MS_CheckSamples, Yes);
+           ui->validSamples->setHidden(true);
+           checkNSamples_ = Yes;
+       }
+       else
+       {
+    //       Client::writePV(MS_CheckSamples, MS_CheckSamples_val);
+           ui->validSamples->setHidden(false);
+           checkNSamples_ = No;
+       }
+       break;
+
+   case 3:
+       if(ui->samples3->text().toInt() == samplesGUI->getCheckCount() and checkSample_ == 1)
+       {
+    //       Client::writePV(MS_CheckSamples, Yes);
+           ui->validSamples3->setHidden(true);
+           checkNSamples_ = Yes;
+       }
+       else
+       {
+    //       Client::writePV(MS_CheckSamples, MS_CheckSamples_val);
+           ui->validSamples3->setHidden(false);
+           checkNSamples_ = No;
+       }
+       break;
    }
 
    switch (experimentType_)
@@ -288,7 +357,7 @@ void Wizard::on_intervals_textEdited(const QString &NInt)
 {
     // Nintervals validation
     if(configFile_ == 1 or loadFile_ ==1)
-        checkIntervals(NInt);
+        checkIntervals(NInt, ui->intervals);
     intervalsTable->enterRows(NInt.toInt());
 }
 
@@ -297,7 +366,7 @@ void Wizard::on_samples_textEdited(const QString &samples)
     // samples validation
 
     if(configFile_ == 1 or loadFile_ ==1)
-        checkSamples(samples);
+        checkSamples(samples, ui->samples);
 }
 
 void Wizard::on_scans_textEdited(const QString &scans)
@@ -305,7 +374,7 @@ void Wizard::on_scans_textEdited(const QString &scans)
     // scans validation
 
     if(configFile_ == 1 or loadFile_ ==1)
-        checkScans(scans);
+        checkScans(scans, ui->scans);
 }
 
 void Wizard::on_expFileName_textEdited(const QString &fileName)
@@ -313,7 +382,7 @@ void Wizard::on_expFileName_textEdited(const QString &fileName)
     // file name validation
 
     if(configFile_ == 1 or loadFile_ ==1)
-        checkExpFileName(fileName);
+        checkExpFileName(fileName, ui->expFileName);
 }
 
 void Wizard::on_settlingTime_textEdited(const QString &settlingTime)
@@ -321,29 +390,39 @@ void Wizard::on_settlingTime_textEdited(const QString &settlingTime)
     // settling time validation
 
     if(configFile_ == 1 or loadFile_ ==1)
-        checkSettlingTime(settlingTime);
+        checkSettlingTime(settlingTime, ui->settlingTime);
 }
 
-void Wizard::checkIntervals(const QString &NInt)
+void Wizard::checkIntervals(const QString &NInt, QLineEdit* lineEdit)
 {
     // Nintervals validation
 
     if(regex_match(NInt.toStdString(), regex("^[1-9][0-9]*$")))
     {
         intervals_ = Yes;    // trigger to nextID function
-        setBorderLineEdit(No, ui->intervals);       // clear the style sheet
+        setBorderLineEdit(No, lineEdit);       // clear the style sheet
         Client::writePV(MS_CheckTable, MS_CheckTable_val);
-        ui->validIntervals->setHidden(false);
+
+        switch (scanningType_) {
+
+        case 1:
+            ui->validIntervals->setHidden(false);
+            break;
+
+        case 3:
+            ui->validIntervals3->setHidden(false);
+            break;
+        }
     }
     else
     {
         intervals_ = No;
-        setBorderLineEdit(Yes, ui->intervals);     // set the style sheet (red border)
+        setBorderLineEdit(Yes, lineEdit);     // set the style sheet (red border)
 //        UImessage(UItittle, "Please enter valid Number of intervals");
     }
 }
 
-void Wizard::checkSamples(const QString &samples)
+void Wizard::checkSamples(const QString &samples, QLineEdit* lineEdit)
 {
     // samples validation
 
@@ -351,43 +430,70 @@ void Wizard::checkSamples(const QString &samples)
     if(samples.toInt() > 0 and samples.toInt() < 41)
     {
         samples_ = Yes;
-        ui->samplesButton->setEnabled(true);
-        setBorderLineEdit(No, ui->samples);       // clear the style sheet
+        switch (scanningType_) {
+
+        case 1:
+            ui->samplesButton->setEnabled(true);
+            break;
+
+        case 3:
+            ui->samplesButton3->setEnabled(true);
+            break;
+        }
+        setBorderLineEdit(No, lineEdit);       // clear the style sheet
     }
     else
     {
         samples_ = No;
-        ui->samplesButton->setEnabled(false);
+        switch (scanningType_) {
 
-        setBorderLineEdit(Yes, ui->samples);     // set the style sheet (red border)
+        case 1:
+            ui->samplesButton->setEnabled(false);
+            break;
+
+        case 3:
+            ui->samplesButton3->setEnabled(false);
+            break;
+        }
+
+        setBorderLineEdit(Yes, lineEdit);     // set the style sheet (red border)
 //        UImessage(UItittle, "Please enter a valid number of Samples, and, make sure to click on the Samples button to keep or change the default values");
     }
 
     if(samples.toInt() != samplesGUI->getCheckCount())
     {
         Client::writePV(MS_CheckSamples, MS_CheckSamples_val);
-        ui->validSamples->setHidden(false);
+        switch (scanningType_) {
+
+        case 1:
+            ui->validSamples->setHidden(false);
+            break;
+
+        case 3:
+            ui->validSamples3->setHidden(false);
+            break;
+        }
     }
 }
 
-void Wizard::checkScans(const QString &scans)
+void Wizard::checkScans(const QString &scans, QLineEdit* lineEdit)
 {
     // scans validation
 
     if(regex_match(scans.toStdString(), regex("^[1-9][0-9]*$")))
     {
         scans_ = Yes;
-        setBorderLineEdit(No, ui->scans);       // clear the style sheet
+        setBorderLineEdit(No, lineEdit);       // clear the style sheet
     }
     else
     {
         scans_ = No;
-        setBorderLineEdit(Yes, ui->scans);     // set the style sheet (red border)
+        setBorderLineEdit(Yes, lineEdit);     // set the style sheet (red border)
 //        UImessage(UItittle, "Please enter a valid number of scans");
     }
 }
 
-void Wizard::checkExpFileName(const QString &fileName)
+void Wizard::checkExpFileName(const QString &fileName, QLineEdit* lineEdit)
 {
     // file name validation
 
@@ -397,40 +503,53 @@ void Wizard::checkExpFileName(const QString &fileName)
 
         configFileName = ui->expFileName->text() + "_" + timeStamp + ".config";
         fullFileName = fileName;
-        setBorderLineEdit(No, ui->expFileName);       // clear the style sheet
+        setBorderLineEdit(No, lineEdit);       // clear the style sheet
     }
     else
     {
         expFileName_ = No;
-        setBorderLineEdit(Yes, ui->expFileName);     // set the style sheet (red border)
+        setBorderLineEdit(Yes, lineEdit);     // set the style sheet (red border)
 //        UImessage(UItittle, "Please enter a valid data file name");
     }
 }
 
-void Wizard::checkSettlingTime(const QString &settlingTime)
+void Wizard::checkSettlingTime(const QString &settlingTime, QLineEdit* lineEdit)
 {
     // settling time validation
 
     if(regex_match(settlingTime.toStdString(), regex("[+]?([0-9]*[.])?[0-9]+")))
     {
         settlingTime_ = Yes;
-        setBorderLineEdit(No, ui->settlingTime);       // clear the style sheet
+        setBorderLineEdit(No, lineEdit);       // clear the style sheet
     }
     else
     {
         settlingTime_ = No;
-        setBorderLineEdit(Yes, ui->settlingTime);       // clear the style sheet
+        setBorderLineEdit(Yes, lineEdit);       // clear the style sheet
 //        UImessage(UItittle, "Please enter a valid settling time");
     }
 }
 
 void Wizard::configFileCheck()
 {
-//    checkIntervals(ui->intervals->text());
-    checkSamples(ui->samples->text());
-    checkScans(ui->scans->text());
-    checkExpFileName(ui->expFileName->text());
-    checkSettlingTime(ui->settlingTime->text());
+    switch (scanningType_) {
+
+    case 1:
+    //    checkIntervals(ui->intervals->text());
+        checkSamples(ui->samples->text(), ui->samples);
+        checkScans(ui->scans->text(), ui->scans);
+        checkExpFileName(ui->expFileName->text(), ui->expFileName);
+        checkSettlingTime(ui->settlingTime->text(), ui->settlingTime);
+        break;
+
+    case 3:
+        checkSamples(ui->samples3->text(), ui->samples3);
+        checkScans(ui->scans3->text(), ui->scans3);
+        checkExpFileName(ui->expFileName3->text(), ui->expFileName3);
+        checkSettlingTime(ui->settlingTime3->text(), ui->settlingTime3);
+        break;
+    }
+
 }
 
 void Wizard::on_loadConfigFileButton_clicked()
@@ -467,23 +586,44 @@ void Wizard::loadConfigFile(const QString& configFile)
     QFile file(configFile);
     if(file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
-        QByteArray jsonData = file.readAll();        // can use QStreamTex, but QByte array easier ti use
+        QByteArray jsonData = file.readAll();        // can use QStreamTex, but QByte array easier to use
         QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData);
 
         QJsonObject jsonObj = jsonDoc.object();
 
-        ui->intervals->setText(jsonObj["NIntervals"].toString());
-        Client::writePV(MS_Intervals, jsonObj["NIntervals"].toString());
-        checkIntervals(ui->intervals->text());
+        switch (scanningType_) {
+
+        case 1:
+            ui->intervals->setText(jsonObj["NIntervals"].toString());
+            Client::writePV(MS_Intervals, jsonObj["NIntervals"].toString());
+            checkIntervals(ui->intervals->text(), ui->intervals);
+
+            ui->samples->setText(jsonObj["NSamples"].toString());
+            ui->scans->setText(jsonObj["Nscans"].toString());
+            ui->expFileName->setText(jsonObj["expFileName"].toString());
+            ui->settlingTime->setText(jsonObj["settlingTime"].toString());
+            ui->userComments->setText(jsonObj["userComments"].toString());
+            ui->expComments->setText(jsonObj["expComments"].toString());
+
+            break;
+
+        case 3:
+            ui->intervals3->setText(jsonObj["NIntervals"].toString());
+            Client::writePV(MS_Intervals, jsonObj["NIntervals"].toString());
+            checkIntervals(ui->intervals3->text(), ui->intervals3);
+
+            ui->samples3->setText(jsonObj["NSamples"].toString());
+            ui->scans3->setText(jsonObj["Nscans"].toString());
+            ui->expFileName3->setText(jsonObj["expFileName"].toString());
+            ui->settlingTime3->setText(jsonObj["settlingTime"].toString());
+            ui->userComments3->setText(jsonObj["userComments"].toString());
+            ui->expComments3->setText(jsonObj["expComments"].toString());
+            break;
+        }
+
 
         intervalsTable->loadIntervalsFromJson(jsonObj["Intervals"].toArray());
-        ui->samples->setText(jsonObj["NSamples"].toString());
         samplesGUI->loadSamplesData(jsonObj["Samples"].toArray());
-        ui->scans->setText(jsonObj["Nscans"].toString());
-        ui->expFileName->setText(jsonObj["expFileName"].toString());
-        ui->settlingTime->setText(jsonObj["settlingTime"].toString());
-        ui->userComments->setText(jsonObj["userComments"].toString());
-        ui->expComments->setText(jsonObj["expComments"].toString());
 
         Client::writePV(MS_Samples, jsonObj["NSamples"].toString());
         Client::writePV(MS_Scans, jsonObj["Nscans"].toString());
@@ -512,19 +652,36 @@ void Wizard::createConfigFile(QString &config)
     {
         QJsonObject jsonObj;
 
+        switch (scanningType_) {
+
+        case 1:
+            jsonObj["NIntervals"]       = ui->intervals->text();
+            jsonObj["NSamples"]         = ui->samples->text();
+            jsonObj["Nscans"]           = ui->scans->text();
+            jsonObj["settlingTime"]     = ui->settlingTime->text();
+            jsonObj["userComments"]     = ui->userComments->text();
+            jsonObj["expComments"]      = ui->expComments->text();
+
+            break;
+
+        case 3:
+            jsonObj["NIntervals"]       = ui->intervals3->text();
+            jsonObj["NSamples"]         = ui->samples3->text();
+            jsonObj["Nscans"]           = ui->scans3->text();
+            jsonObj["settlingTime"]     = ui->settlingTime3->text();
+            jsonObj["userComments"]     = ui->userComments3->text();
+            jsonObj["expComments"]      = ui->expComments3->text();
+
+            break;
+        }
+
         jsonObj["expType"]          = experimentTypeS;
         jsonObj["scanningType"]     = scanningTypeS;
         jsonObj["loadedConfig"]     = configFileS;
-        jsonObj["NIntervals"]       = ui->intervals->text();
         jsonObj["Intervals"]        = intervalsTable->createIntervalsJson();
-        jsonObj["NSamples"]         = ui->samples->text();
         jsonObj["Samples"]          = samplesGUI->getSamplesData();
-        jsonObj["Nscans"]           = ui->scans->text();
         jsonObj["expFileName"]      = fullFileName;
-        jsonObj["settlingTime"]     = ui->settlingTime->text();
         jsonObj["robotInUse"]       = robotInUseS;
-        jsonObj["userComments"]     = ui->userComments->text();
-        jsonObj["expComments"]      = ui->expComments->text();
 
         QJsonDocument jsonDoc(jsonObj);
         file.write(jsonDoc.toJson());
@@ -582,4 +739,49 @@ void Wizard::keyPressEvent(QKeyEvent *event)
 void Wizard::closeEvent(QCloseEvent *event)
 {
     event->ignore();       // Ignore the close event
+}
+
+void Wizard::on_intervals3_textEdited(const QString &arg1)
+{
+    // Nintervals validation
+    if(configFile_ == 1 or loadFile_ ==1)
+        checkIntervals(arg1, ui->intervals3);
+    intervalsTable->enterRows(arg1.toInt());
+}
+
+void Wizard::on_samples3_textEdited(const QString &arg1)
+{
+    // samples validation
+
+    if(configFile_ == 1 or loadFile_ ==1)
+        checkSamples(arg1, ui->samples3);
+}
+
+void Wizard::on_scans3_textEdited(const QString &arg1)
+{
+    // scans validation
+
+    if(configFile_ == 1 or loadFile_ ==1)
+        checkScans(arg1, ui->scans3);
+}
+
+void Wizard::on_expFileName3_textEdited(const QString &arg1)
+{
+    // file name validation
+
+    if(configFile_ == 1 or loadFile_ ==1)
+        checkExpFileName(arg1, ui->expFileName3);
+}
+
+void Wizard::on_settlingTime3_textEdited(const QString &arg1)
+{
+    // settling time validation
+
+    if(configFile_ == 1 or loadFile_ ==1)
+        checkSettlingTime(arg1, ui->settlingTime3);
+}
+
+void Wizard::on_samplesButton3_clicked()
+{
+    samplesGUI->show();
 }
