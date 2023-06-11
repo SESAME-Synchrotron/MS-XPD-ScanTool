57e6fc8e94db0d46b9c4781c71fc97c635751588
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-06-11 10:06:32 +0300

    add twoThetaSlits IOC files

---
+++ IOCs/MS_DAQ/iocBoot/iocMS_TwoThetaStepSlits/st.cmd
@@ -0,0 +1,16 @@
+#< envPaths
+
+epicsEnvSet("IOC","iocMS")
+epicsEnvSet("TOP","/home/dcasu/XRD-Scan/IOCs/MS_DAQ")
+epicsEnvSet("SUPPORT","/opt/epics/support")
+epicsEnvSet("EPICS_BASE","/opt/epics/base")
+epicsEnvSet("P", "MS:")
+
+
+## Register all support components
+dbLoadDatabase "dbd/MS.dbd"
+MS_registerRecordDeviceDriver pdbbase
+
+dbLoadTemplate("/home/dcasu/XRD-Scan/IOCs/MS_DAQ/iocBoot/iocMS_TwoThetaStepSlits/MS_TwoThetaStepSlits.substitutions")
+
+iocInit
