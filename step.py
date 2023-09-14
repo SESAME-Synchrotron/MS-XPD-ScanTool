1971cd03d9989f75aa875acd2d8f771402908a11
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-09-14 12:58:37 +0300

    initial test on robot

--- step.py
+++ step.py
@@ -4,9 +4,9 @@ class step(XPD):
 	def __init__(self, PVsFiles, macros, scanningSubs):
 		super().__init__(PVsFiles, macros, scanningSubs)
 
-		self.preCheck()
-		self.initDir()
-		self.detectorInit()
+		# self.preCheck()
+		# self.initDir()
+		# self.detectorInit()
 
 	def preCheck(self):
 		super().preCheck()
