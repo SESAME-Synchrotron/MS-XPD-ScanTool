5ea193bacd7b66f6698e5d2efc97eb8b949c49b0
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-09-19 11:29:44 +0300

    add twoThetaTemp IOC, and prerequisites

---
+++ twoThetaTemp.py
@@ -0,0 +1,9 @@
+import log
+import time
+from step import step
+from robot import robot
+from SEDSS.CLIMessage import CLIMessage
+
+class twoThetaTemp(step):
+	def __init__(self, PVsFiles, macros, scanningSubs):
+		super().__init__(PVsFiles, macros, scanningSubs)
