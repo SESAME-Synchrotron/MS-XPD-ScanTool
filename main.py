db5419348d4ff8d5d6edd70e59320957a1e5a559
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-10-26 17:24:47 +0300

    check if the UI not found, check if the UI_IOC not running, and send emails notifications

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import epics
 from twoThetaStep import twoThetaStep
 from twoThetaTemp import twoThetaTemp
 from twoThetaStepSlits import twoThetaStepSlits
+from emailNotifications import email
 from SEDSS.CLIMessage import CLIMessage
 from SEDSS.SEDFileManager import readFile
 
@@ -36,6 +37,7 @@ _3 = scanningSubs["_3"]
 reqFiles = configFile["files"]["reqFiles"]
 MS_req                   = reqFiles["MS"]
 MS_UI_req                = reqFiles["MS_UI"]
+MS_supp_req              = reqFiles["MS_Support"]
 MS_TwoThetaStep_req      = reqFiles[MS_TwoThetaStep]
 MS_TwoThetaTemp_req      = reqFiles[MS_TwoThetaTemp]
 MS_TwoThetaStepSlits_req = reqFiles[MS_TwoThetaStepSlits]
@@ -46,21 +48,29 @@ MS_TwoThetaStep_exe      = _EXE[MS_TwoThetaStep]
 MS_TwoThetaTemp_exe      = _EXE[MS_TwoThetaTemp]
 MS_TwoThetaStepSlits_exe = _EXE[MS_TwoThetaStepSlits]
 
-pvlist = [_TOP + MS_UI_req, _TOP + MS_req]
+pvlist = [_TOP + MS_UI_req, _TOP + MS_req, _TOP + MS_supp_req]
 macros = {P:prefix, N:list(range(1, 41))}
 
 if __name__ == "__main__":
+
+	scanningToolPV = epics.PV(scanningTool_PV).get(timeout=1, use_monitor=False)
+	
+	if scanningToolPV is None:
+		CLIMessage("Scanning IOC is not running!!", "E")
+		email("").sendEmail("UI_IOC")
+		sys.exit()
 
 	if os.path.exists(DAQ_Tool):
 		os.system(f'cd & {DAQ_Tool} -qt')
 	else:
 		CLIMessage(f"Can't start the scanning tool! {DAQ_Tool}", "E")
+		email("").sendEmail(type="UI", msg=f"UI path: {DAQ_Tool}")
 		sys.exit()
 
 	scanningToolPV = epics.PV(scanningTool_PV).get(timeout=1, use_monitor=False)
 
 	if scanningToolPV == 1:
-		if not os.system(f"tmux has-session -t {MS_TwoThetaStep}") == 0:
+		if os.system(f"tmux has-session -t {MS_TwoThetaStep}") != 0:
 			command = f'tmux new -d -s {MS_TwoThetaStep} && tmux send-keys -t {MS_TwoThetaStep} "cd {_IOC}; {MS_TwoThetaStep_exe}" ENTER'
 			os.system(command)
 			sleep(0.5)
@@ -68,7 +78,7 @@ if __name__ == "__main__":
 		twoThetaStep(pvlist, macros, _1)
 
 	elif scanningToolPV == 2:
-		if not os.system(f"tmux has-session -t {MS_TwoThetaTemp}") == 0:
+		if os.system(f"tmux has-session -t {MS_TwoThetaTemp}") != 0:
 			command = f'tmux new -d -s {MS_TwoThetaTemp} && tmux send-keys -t {MS_TwoThetaTemp} "cd {_IOC}; {MS_TwoThetaTemp_exe}" ENTER'
 			os.system(command)
 			sleep(0.5)
@@ -76,7 +86,7 @@ if __name__ == "__main__":
 		twoThetaTemp(pvlist, macros, _2)
 
 	elif scanningToolPV == 3:
-		if not os.system(f"tmux has-session -t {MS_TwoThetaStepSlits}") == 0:
+		if os.system(f"tmux has-session -t {MS_TwoThetaStepSlits}") != 0:
 			command = f'tmux new -d -s {MS_TwoThetaStepSlits} && tmux send-keys -t {MS_TwoThetaStepSlits} "cd {_IOC}; {MS_TwoThetaStepSlits_exe}" ENTER'
 			os.system(command)
 			sleep(0.5)
