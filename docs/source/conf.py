a14949aafc4901728d51a86edd9d319196d99c83
M1Abdellatief <149112148+M1Abdellatief@users.noreply.github.com>
2024-09-24 11:39:18 +0300

    change project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,9 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 Affiliation = u'SESAME'
-project = 'MS/XPD Scanning Tool'
+project = 'MS/XPD Beamline Docs'
 copyright = u'2024, ' + Affiliation
-author = 'SESAME DCA and MS Teams'
+author = 'SESAME Teams'
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
@@ -40,7 +40,7 @@ texinfo_documents = [
   project+u' Documentation',
   Affiliation, 
   project, 
-  'ID09 - MS/XPD (Materials Science/X-ray Powder Diffraction) Beamline DAQ System'),
+  'ID09 - MS/XPD (Materials Science/X-ray Powder Diffraction) Beamline Docs'),
 ]
 
 # -- General configuration ---------------------------------------------------
